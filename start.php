51fa38c1b771660976f1941bce49262e3ec49e24
Evan Winslow <evan@elgg.org>
2011-03-18 20:09:55 -0700

    Updated for 1.8

--- start.php
+++ start.php
@@ -1,26 +1,22 @@
 <?php
 
-function minify_init()
-{
+function minify_init() {
 	//make sure this runs after everyone else is done
-	register_plugin_hook('display', 'view', 'minify_views', 1000);
+	elgg_register_plugin_hook('view', 'all', 'minify_views', 1000);
 }
 
-function minify_views($hook, $type, $content, $params)
-{
+function minify_views($hook, $type, $content, $params) {
 	$view = $params['view'];
-	
+
 	if (preg_match("/^js\//", $view)) {
-		if (include_once dirname(__FILE__).'/lib/min/lib/JSMin.php') {
+		if (include_once dirname(__FILE__) . '/vendors/min/lib/JSMin.php') {
 			return JSMin::minify($content);
 		}
-	} elseif ($view === 'css') {
-		if (include_once('lib/min/lib/CSS.php')) {
+	} elseif (preg_match("/^css\//", $view)) {
+		if (include_once dirname(__FILE__) . '/vendors/min/lib/CSS.php') {
 			return Minify_CSS::minify($content);
 		}
 	}
 }
 
-register_elgg_event_handler('init', 'system', 'minify_init');
-
-?>
+elgg_register_event_handler('init', 'system', 'minify_init');
